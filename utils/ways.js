9ac8595a6b82e1cb8d053f39a51df0de7f8e79b4
shuiyi <1006532586@qq.com>
2019-03-05 10:31:19 +0800

    mip start

--- utils/ways.js
+++ utils/ways.js
@@ -1,10 +1,21 @@
+const marequest = require('marequest.js')
 
 
-
-
+// 
+function mipData() {
+    return new Promise(resolve => {
+        marequest.request({
+            url: 'api/Users/GetUserData',
+            enableUserInfo: true,
+            complete: function (res) {
+                res && resolve(res);
+            }
+        })
+    })
+}
 
 
 
 export default {
-
+    mipData
 }
